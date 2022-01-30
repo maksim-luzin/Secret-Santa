9ca2f09b430af9b33e59dfe641183bbb91a59427
maksim-luzin <333maximax333@gmail.com>
2022-01-30 15:26:12 +0200

    Add frontend template

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './common/containers/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
