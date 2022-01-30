c6df69ad19e776e634e77c7b3964a62b4b6160a7
maksim-luzin <333maximax333@gmail.com>
2022-01-30 17:54:41 +0200

    Add frontend helpers

--- frontend/src/common/helpers/index.ts
+++ frontend/src/common/helpers/index.ts
@@ -2,6 +2,5 @@ import { getToken } from "./getToken";
 import { setToken } from "./setToken";
 import { api } from "./apiHelper";
 import { createReducer } from "./createReducer";
-import { isAuth } from "./isAuth";
 
-export { getToken, setToken, api, createReducer, isAuth };
+export { getToken, setToken, api, createReducer };
