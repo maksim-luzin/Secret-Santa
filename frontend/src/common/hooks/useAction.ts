c33d18089537255c5c33bf7b6d4ab673d5c4bc4c
maksim-luzin <333maximax333@gmail.com>
2022-01-31 16:49:44 +0200

    Edit frontend hook

--- frontend/src/common/hooks/useAction.ts
+++ frontend/src/common/hooks/useAction.ts
@@ -1,12 +1,9 @@
-import { useMemo } from 'react';
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as actions from '../actions';
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../actions";
 
 export const useAction = () => {
   const dispatch = useDispatch();
-  return useMemo(
-    () => bindActionCreators(actions, dispatch),
-    [actions]
-  );
+  return useMemo(() => bindActionCreators(actions, dispatch), [dispatch]);
 };
