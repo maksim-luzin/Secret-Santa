5ee83686118438fb3c687d39c86805a9c800a236
maksim-luzin <333maximax333@gmail.com>
2022-01-31 22:31:09 +0200

    Add server config.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import http from 'http';
+import { createServer } from 'http';
+import { readFileSync } from 'fs';
 import express from 'express';
 import bodyParser from 'body-parser';
-import passport from 'passport';
+import pasport from 'passport';
 import cors from 'cors';
 import { Server } from 'socket.io';
 import { env } from './env';
@@ -14,20 +15,40 @@ import socketHandlers from './api/socket/handlers';
 import './config/passportConfig';
 import { Routes } from './common/enums';
 
+const production = process?.env?.NODE_ENV === 'production' || false;
+
+const staticPath = production
+  ? './build/client'
+  : `${__dirname}/../../frontend/build`;
+
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server);
 
 app.use(cors());
 io.on('connection', socketHandlers);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
+app.use(pasport.initialize());
 app.use(socketInjector(io));
 
 app.use(Routes.API, authorizationMiddleware(routesWhiteList));
 
 routes(app);
+
+app.use((req, res, next) => {
+  res.set({
+    'Cache-Control': ['max-age=34164000', 'public'],
+  });
+  next();
+});
+
+app.use(express.static(staticPath));
+
+app.get('*', (req, res) => {
+  res.write(readFileSync(`${staticPath}/index.html`));
+  res.end();
+});
 
 app.use(errorHandlerMiddleware);
 
@@ -35,3 +56,5 @@ server.listen(env.app.port, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running at ${env.app.port}.`);
 });
+
+export default app;
