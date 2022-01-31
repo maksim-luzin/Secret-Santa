73ec13e963789200186e4dfe762258c04197ff74
maksim-luzin <333maximax333@gmail.com>
2022-01-31 16:50:45 +0200

    Edit frontend routing

--- frontend/src/common/containers/Routing/index.tsx
+++ frontend/src/common/containers/Routing/index.tsx
@@ -7,29 +7,37 @@ import { Auth } from '../../../scenes/Auth/components/Auth';
 import { Play } from '../../../scenes/Play/components/Play';
 import { WishList } from '../../../scenes/WishList/components/WishList';
 import { NotFound } from '../../components/NotFound';
+import { Notifications } from '../../components/Notifications';
 import { getToken } from '../../helpers';
 import { useAction } from '../../hooks/useAction';
 import { useAuth } from '../../hooks/useAuth';
+import { useTypedSelector } from '../../hooks/store';
+import { WsHelper } from '../../helpers';
 
 const Routing = () => {
   const { authorizedAction } = useAction();
   const token = getToken();
   const { isAuthorized } = useAuth();
+  const message = useTypedSelector(root => root.authentication.error || root.wish.error || '');
 
   useEffect(() => {
     if (!isAuthorized && token) {
       authorizedAction();
-      console.log(111);
     }
   }, [isAuthorized, token, authorizedAction]);
 
   return (
-    <Switch>
-      <PublicRoute isAuthorized={isAuthorized} path={Routes.AUTH} component={Auth} />
-      <PrivateRoute isAuthorized={isAuthorized} path={Routes.PLAY} component={Play} />
-      <PrivateRoute isAuthorized={isAuthorized} path={Routes.WISHLIST} component={WishList} />
-      <Route path="*" component={NotFound} />
-    </Switch>
+    <>
+      <Switch>
+        <PublicRoute isAuthorized={isAuthorized} exact path={Routes.AUTH} component={Auth} />
+        <PrivateRoute isAuthorized={isAuthorized} exact path={Routes.WISHLIST} component={WishList} />
+        <PrivateRoute isAuthorized={isAuthorized} exact path={Routes.PLAY} component={Play} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+      <Notifications message={message} />
+      <WsHelper />
+    </>
+
   );
 };
 
